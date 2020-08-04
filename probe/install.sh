3a4c70487eae3e1f9ac9ede05c2b97ba219b3358
Cyrill Leutwiler <bigcyrill@hotmail.com>
2020-08-04 21:05:51 +0200

    update probe install script

--- probe/install.sh
+++ probe/install.sh
@@ -12,8 +12,17 @@ if [ "$1" == "uninstall" ] || [ "$1" == "-u" ] ; then
 
 	echo "Removing $TARGET_FILE"
 	sudo rm $TARGET_FILE
+
+	echo "Script and service unit removed; please uninstall the dependencies manually"
 	exit 0
 fi
+
+echo "Installing system dependencies"
+sudo apt update
+sudo apt install python3-pip python3-setuptools python3-wheel build-essential python-dev
+
+echo "Installing python dependencies"
+sudo pip3 install -r requirements.txt
 
 echo "Installing into $TARGET_FILE"
 sudo cp probe.py $TARGET_FILE
